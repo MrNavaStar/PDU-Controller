44f616149ed278a0064a888efc836ebeb2341618
MrNavaStar <Mr.NavaStar@gmail.com>
2024-01-05 00:42:33 -0700

    testing ups

--- ups.py
+++ ups.py
@@ -11,4 +11,12 @@ class UPS:
         result = subprocess.run(['upsc', self.url], stdout=subprocess.PIPE)
         for line in result.stdout.decode('utf-8').replace("Init SSL without certificate database", "").split("\n"):
             data = line.split(": ")
+            if len(data) < 2:
+                continue
             info[data[0]] = data[1]
+        return info
+
+
+if __name__ == '__main__':
+    ups = UPS("cyberpower@localhost")
+    print(ups.info())
